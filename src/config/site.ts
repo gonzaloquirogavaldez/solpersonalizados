9306fa77de0fab706c5103e63345aaa1aa06ddc8
Gonzalo Quiroga Valdez <gonqui19@gmail.com>
2026-09-10 22:26:38 -0300

    primer subida

---
+++ src/config/site.ts
@@ -0,0 +1,8 @@
+// Cambia estos valores cuando tengas el correo institucional y el proveedor de formularios.
+export const gym = {
+  name: 'Sol Personalizados',
+  shortName: 'SOL',
+  instagram: 'https://www.instagram.com/sol.chdpersonalizados/',
+  institutionalEmail: 'COMPLETAR_CORREO_INSTITUCIONAL',
+  formEndpoint: 'https://formsubmit.co/COMPLETAR_CORREO_INSTITUCIONAL',
+};
