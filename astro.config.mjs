fa97ae5799e470722e4cf4fbe9ae6f70d1baae53
Gonzalo Quiroga Valdez <gonqui19@gmail.com>
2026-09-11 07:56:02 -0300

    error 403

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://solpersonalizados.com.ar',
   output: 'server',
   adapter: node({ mode: 'standalone' }),
 });
